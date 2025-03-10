628199e285cbb7c0460bd5f3b73d0c17b1036c54
Koh Jun Sheng <jskoh.2023@scis.smu.edu.sg>
2025-03-11 01:24:58 +0800

    tidyup

--- test/CDTest.java
+++
@@ -1,8 +0,0 @@
-public class CDTest {
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        System.out.println(deck.getTopCard());
-        System.out.println(deck.getTopCard());
-        System.out.println(deck.getTopCard());
-    }
-}
