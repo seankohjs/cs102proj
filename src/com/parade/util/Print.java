032702bdefc97abb9cc1da90b7f792f61b46d13b
jlroch2024 <jl.roch.2024@smu.edu.sg>
2025-04-10 01:36:19 +0800

    Print

--- src/com/parade/util/Print.java
+++ src/com/parade/util/Print.java
@@ -17,6 +17,16 @@ public class Print {
     public static final String PURPLE = "\u001B[1;35m";
     public static final String GREY = "\u001B[1;37m";
     public static final String YELLOW = "\033[1;33m";
+    public static final String CYAN = "\033[1;36m";
+
+    // public static final String RED = "\u001B[1;38;5;160m";
+    // public static final String BLUE = "\u001B[38;5;27m";
+    // public static final String GREEN = "\u001B[1;38;5;34m";
+    // public static final String ORANGE = "\033[1;38;5;208m";
+    // public static final String PURPLE = "\u001B[1;38;5;129m";
+    // public static final String GREY = "\u001B[1;38;5;246m";
+    // public static final String YELLOW = "\u001B[1;38;5;184m";
+
 
     // CARD BOX DRAWING
     public static final String HORIZONTAL = "\u2550"; // ═
